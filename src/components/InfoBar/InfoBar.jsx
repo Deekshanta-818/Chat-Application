c19050801623a376a750f18a27d67be189abdf5f
Deekshanta-818 <deekshantapaudel@gmail.com>
2020-10-10 18:57:31 +0545

    My commit

--- src/components/InfoBar/InfoBar.jsx
+++ src/components/InfoBar/InfoBar.jsx
@@ -11,7 +11,7 @@ const InfoBar = ({ room }) => (
     <div className="leftInnerContainer">
       <img className="onlineIcon" src={onlineIcon} alt="online icon" />
       <h3>{room}</h3>
-      <h3 style={{marginLeft:'80px',color:'green'}} className='emoji'>Chat with anyone you like🥰😎😊</h3>
+      <h3 style={{marginLeft:'80px',color:'green'}} className='emoji'>Chat with anyone you like<span role='img' aria-label="emoji" >🥰</span><span role='img' aria-label="emoji" >😎</span><span role='img' aria-label="emoji" >😊</span></h3>
     </div>
     <div className="rightInnerContainer">
       <a href="/"><img src={closeIcon} alt="close icon" /></a>
