8c1cbc82f3132a8cc127e79942951f10642df784
Deekshanta-818 <deekshantapaudel@gmail.com>
2020-10-10 18:46:54 +0545

    First commit

---
+++ src/components/InfoBar/InfoBar.jsx
@@ -0,0 +1,22 @@
+  
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+import closeIcon from '../../icons/closeIcon.png';
+
+import './InfoBar.css';
+
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <h3>{room}</h3>
+      <h3 style={{marginLeft:'80px',color:'green'}} className='emoji'>Chat with anyone you like🥰😎😊</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
